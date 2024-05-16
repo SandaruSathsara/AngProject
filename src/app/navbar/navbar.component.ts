549aafa5983248c86f80f6e0449404753f44ea20
SandaruSathsara <sssfernando@students.nsbm.ac.lk>
2024-05-16 11:42:41 +0530

    chnages

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
