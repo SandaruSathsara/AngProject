dfe5c8ea2806860779f425bd6fa87e855e0baf66
SandaruSathsara <sssfernando@students.nsbm.ac.lk>
2024-05-16 11:42:54 +0530

    changes 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UsersComponent } from './users/users.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent,UsersComponent,MatSlideToggleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UsersComponent } from './users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '/',
+        title: 'App Navbar',
+        component: NavbarComponent,
+      },
+
+      {
+        path: '/users',
+        title: 'Users',
+        component: UsersComponent,  
+      },
+];
